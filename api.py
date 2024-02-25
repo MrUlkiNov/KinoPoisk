8a674c589167cc6eedeea40cd29b82627c628575
MrUlkiNov <sam.baybekov@mail.ru>
2024-02-25 18:19:00 +0300

    Delete fatals and extra files

--- api.py
+++ api.py
@@ -1,10 +1,11 @@
+from typing import Any
 import requests
 from pprint import pprint
 from config import API_TOKEN
 import json
 
 
-def get_params(country: str, count: int) -> dict:
+def get_params(year: str, count: int) -> str | Any:
     """
     Функция принимает параметр отсеивания Страна и так же количество данных, возвращает словарь с API сервера
     """
@@ -14,9 +15,11 @@ def get_params(country: str, count: int) -> dict:
     response = requests.get(
         f"https://api.kinopoisk.dev/v1.4/movie?page=1&limit={count}&selectFields=name&selectFields"
         f"=rating&selectFields=year&selectFields=poster&selectFields=genres&selectFields"
-        f"=countries&notNullFields=name&notNullFields=type&countries.name={country}", headers=headers)
-    info = json.loads(response.text)
-    return info
+        f"=countries&notNullFields=name&notNullFields=type&year={year}", headers=headers)
+    if response.status_code != 200:
+        return "Не удалось выполнить поиск"
+    else:
+        return json.loads(response.text)
 
 
 def print_info(params: dict, sort_type: int) -> None:
@@ -30,14 +33,16 @@ def print_info(params: dict, sort_type: int) -> None:
         result = sorted(params['docs'], key=lambda x: x['year'])
     pprint(result)
 
+
 def client_window() -> None:
     """
     Основная функция, которая принимает от пользователя параметры и вызывает функции для вывода данных
     """
-    sort_type = int(input("1 - low, 2 - high: "))
-    country = input("Введите страну фильма: ")
-    count = int(input("Сколько данных необходимо вывести? "))
-    params = get_params(country, count)
+    sort_type = int(input("1 - low, 2 - high (low - От новых фильмов до старых) (high - От старых к новым): "))
+    year = input("Введите диапазон годов фильма (Пример: 2005-2010): ")
+    count = int(input("Сколько фильмов необходимо вывести? "))
+    params = get_params(year, count)
     print_info(params, sort_type)
+
 
 client_window()
