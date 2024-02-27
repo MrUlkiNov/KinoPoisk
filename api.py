444d51458fa8b3f674cd211beeeea98f3f9da46d
MrUlkiNov <sam.baybekov@mail.ru>
2024-02-27 22:15:57 +0300

    Added a low function

--- api.py
+++ api.py
@@ -5,44 +5,55 @@ from config import API_TOKEN
 import json
 
 
-def get_params(year: str, count: int) -> str | Any:
+def get_params(sort_type: str, count: int) -> str | Any:
     """
     Функция принимает параметр отсеивания Страна и так же количество данных, возвращает словарь с API сервера
     """
+    global start_year, end_year
     headers = {
         "X-API-KEY": API_TOKEN
     }
+    if sort_type.lower() == 'low':
+        start_year = 1970
+        end_year = 1990
+    elif sort_type.lower() == 'high':
+        start_year = 2019
+        end_year = 2023
     response = requests.get(
         f"https://api.kinopoisk.dev/v1.4/movie?page=1&limit={count}&selectFields=name&selectFields"
-        f"=rating&selectFields=year&selectFields=poster&selectFields=genres&selectFields"
-        f"=countries&notNullFields=name&notNullFields=type&year={year}", headers=headers)
+        f"=rating&selectFields=year&selectFields=poster&rating.kp=8-10&selectFields=genres&selectFields"
+        f"=&notNullFields=name&notNullFields=type&year={start_year}-{end_year}&selectFields=shortDescription&selectFields=countries",
+        headers=headers)
     if response.status_code != 200:
         return "Не удалось выполнить поиск"
     else:
         return json.loads(response.text)
 
 
-def print_info(params: dict, sort_type: int) -> None:
+def client_window(sort_type: str, count: int) -> str | list[Any]:
     """
-    Функция принимает словарь данных, а так же тип сортировки и выводит отсортированный словарь
+    Основная функция, которая принимает от пользователя параметры и возвращает отсортированный список фильмов
     """
-    global result
-    if sort_type == 1:
-        result = sorted(params['docs'], key=lambda x: x['year'], reverse=True)
-    elif sort_type == 2:
-        result = sorted(params['docs'], key=lambda x: x['year'])
-    pprint(result)
+    global genres, image, country
+    try:
+        params = get_params(sort_type, count)
+        sorted_films = sorted(params['docs'], key=lambda x: x['year'])
+        result_films = []
+        for film in sorted_films:
+            if 'countries' in film:
+                country = film['countries'][0]['name']
+            if 'genres' in film:
+                genres = [i['name'] for i in film['genres']]
+            if 'poster' in film:
+                image = film['poster']['previewUrl']
+            result_films.append(("{} ({}) - Рейтинг: {}\nЖанры:\n{}\nСтрана: {}\n==={}===".format(film['name'], film['year'],
+                                                                                      film['rating']['kp'],
+                                                                                      '\\ '.join(genres),country,
+                                                                                      film['shortDescription']), image))
+        return result_films
+
+    except ValueError:
+        return []
 
 
-def client_window() -> None:
-    """
-    Основная функция, которая принимает от пользователя параметры и вызывает функции для вывода данных
-    """
-    sort_type = int(input("1 - low, 2 - high (low - От новых фильмов до старых) (high - От старых к новым): "))
-    year = input("Введите диапазон годов фильма (Пример: 2005-2010): ")
-    count = int(input("Сколько фильмов необходимо вывести? "))
-    params = get_params(year, count)
-    print_info(params, sort_type)
-
-
-client_window()
+pprint(client_window('low', 3))
--- main.py
+++ main.py
@@ -1,18 +1,50 @@
 import telebot
 from config import BOT_TOKEN
+from telebot import types
+from api import client_window
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
+
 @bot.message_handler(commands=['start', 'hello'])
-def start_window(message):
+def handle_start(message):
     bot.send_message(message.chat.id, "Привет {}! Я виртуальный ассистент. Я отвечу на ваши вопросы о компании, "
-                                      "нашей продукции, помогу сделать заказ. Какой у вас вопрос?".format(message.from_user.first_name))
+                                      "нашей продукции, помогу сделать заказ. Какой у вас вопрос?".format(
+        message.from_user.first_name))
+    show_sort_buttons(message)
 
-@bot.message_handler()
-def info(message):
-    if message.text.lower() == "привет":
-        bot.send_message(message.chat.id, "Привет {}! Я виртуальный ассистент. Я отвечу на ваши вопросы о компании, "
-                                          "нашей продукции, помогу сделать заказ. Какой у вас вопрос?".format(
-            message.from_user.first_name))
 
-bot.infinity_polling()
+@bot.message_handler(func=lambda message: message.text.lower() == 'привет')
+def hello(message):
+    bot.send_message(message.chat.id, "Привет {}! Я виртуальный ассистент. Я отвечу на ваши вопросы о компании, "
+                                      "нашей продукции, помогу сделать заказ. Какой у вас вопрос?".format(
+        message.from_user.first_name))
+    show_sort_buttons(message)
+
+
+def show_sort_buttons(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    button_low = types.KeyboardButton("/low")
+    markup.add(button_low)
+    bot.send_message(message.chat.id, "Пожалуйста, выберите одну из команд:\n/low "
+                                      "- Выбрать старые фильмы с высоким рейтингом", reply_markup=markup)
+
+
+@bot.message_handler(func=lambda message: message.text.lower() == '/low')
+def low(message):
+    bot.send_message(message.chat.id, 'Напишите количество фильмов: ')
+    bot.register_next_step_handler(message, get_count_and_send_low_films)
+
+
+@bot.message_handler(func=lambda message: message.text.lower() == 'low')
+def get_count_and_send_low_films(message):
+    try:
+        count = int(message.text.strip())
+        films = client_window('low', count)
+        for film in films:
+            bot.send_photo(message.chat.id, film[1], caption=film[0])
+    except ValueError:
+        bot.send_message(message.chat.id, "Некорректное количество. Введите целое число.")
+
+
+bot.polling(none_stop=True)
